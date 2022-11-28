9b4c5bc5ab09977843dcb8a19767eca43449cdac
butterriolu <derpvin12@gmail.com>
2022-11-27 22:56:01 -0500

    updated response with total nutrients

--- Controllers/Utils.cs
+++ Controllers/Utils.cs
@@ -3,11 +3,26 @@ namespace McDonalds.Utilities;
 
 public static class Utils
 {
+  public static ComboResponse createComboResponse(string name, float price, List<Food> foods)
+  {
+    ComboResponse result = new ComboResponse { name = name, price = price, foods = foods };
+    foods.ForEach(food =>
+    {
+      result.calories += food.calories;
+      result.protein += food.protein;
+      result.carbs += food.carbs;
+      result.sodium += food.sodium;
+      result.sugar += food.sugar;
+      result.fat += food.fat;
+    });
+
+    return result;
+  }
   // helper function to simulate groupby
-  public static List<AllCombosResponse> GroupByFood(List<ComboFoodPair> comboFoodPairs)
+  public static List<ComboResponse> GroupByFood(List<ComboFoodPair> comboFoodPairs)
   {
     Dictionary<int, int> indexMapping = new Dictionary<int, int>();
-    List<AllCombosResponse> result = new List<AllCombosResponse>();
+    List<ComboResponse> result = new List<ComboResponse>();
 
     int rows = 0;
     comboFoodPairs.ForEach(pair =>
@@ -19,11 +34,17 @@ public static class Utils
       {
         int comboIndex = indexMapping[currCombo.Id];
         result[comboIndex].foods!.Add(currFood);
+        result[comboIndex].calories += currFood.calories;
+        result[comboIndex].protein += currFood.protein;
+        result[comboIndex].carbs += currFood.carbs;
+        result[comboIndex].sodium += currFood.sodium;
+        result[comboIndex].sugar += currFood.sugar;
+        result[comboIndex].fat += currFood.fat;
       }
 
       else
       {
-        result.Add(new AllCombosResponse { combo = currCombo, foods = new List<Food> { currFood } });
+        result.Add(createComboResponse(currCombo.name!, currCombo.price, new List<Food> { currFood }));
         indexMapping[currCombo.Id] = rows++;
       }
 
