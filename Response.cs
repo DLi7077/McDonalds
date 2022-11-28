470a3c699b2de94bad114c89b4455cac10f25c68
butterriolu <derpvin12@gmail.com>
2022-11-27 20:55:02 -0500

    restructure

--- Response.cs
+++ Response.cs
@@ -1,3 +1,7 @@
 namespace McDonalds;
 // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/record
-public record Response<Any>(int status_code, string? description, Any? result);
+public record Response<Any>(
+  int status_code,
+  string? description,
+  Any? result
+);
